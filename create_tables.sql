7b862817ae80afc71e6a5cc1a036f88cce72f5ce
joacoszterensus <jszterensus@udesa.edu.ar>
2025-09-16 18:33:31 -0300

    falta lo de disponibilidad. cache no se hace cargo

--- create_tables.sql
+++ create_tables.sql
@@ -4,7 +4,7 @@ CREATE DATABASE clases_particulares_db;
 -- \c clases_db;
 
 CREATE TABLE usuarios (
-    id_usuario INT PRIMARY KEY,
+    id_usuario SERIAL PRIMARY KEY,
     dni INT UNIQUE NOT NULL,
     nombre VARCHAR(50) NOT NULL,
     apellido VARCHAR(50) NOT NULL,
@@ -13,21 +13,25 @@ CREATE TABLE usuarios (
 );
 
 CREATE TABLE estudiante (
-    id_estudiante INT PRIMARY KEY REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    id_estudiante INT PRIMARY KEY 
     nivel_academico VARCHAR(20) NOT NULL,
     edad INT
+
+    FOREIGN KEY (id_estudiante) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
 );
 
 CREATE TABLE profesor (
-    id_profesor INT PRIMARY KEY REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    id_profesor INT PRIMARY KEY
     telefono INT UNIQUE,
     años_experiencia INT,
     tarifa INT NOT NULL, -- (podría ser un float tmb)
     dni INT UNIQUE NOT NULL
+
+    FOREIGN KEY (id_profesor) REFERENCES usuarios(id_usuario) ON DELETE CASCADE
 );
 
 CREATE TABLE grupo (
-    id_grupo INT PRIMARY KEY,
+    id_grupo SERIAL PRIMARY KEY,
     nombre VARCHAR(50)
 );
 
@@ -42,7 +46,7 @@ CREATE TABLE estudiantePerteneceGrupo (
 );
 
 CREATE TABLE materia (
-    id_materia INT PRIMARY KEY,
+    id_materia SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     nivel VARCHAR(20)
 );
@@ -63,14 +67,55 @@ CREATE TABLE disponibilidad (
 CREATE TABLE profesorTieneDisponibilidad (
 );
 
-CREATE TABLE clase (
+CREATE TABLE Clase (
+  id_clase SERIAL PRIMARY KEY,
+
+  duracion INT,         
+  modalidad VARCHAR(50),   
+  estado VARCHAR(50),
+  id_profesor INT,
+
+  FOREIGN KEY (id_profesor) REFERENCES Profesor(id_usuario)
 );
 
-CREATE TABLE inscripcion (
+
+CREATE TABLE Inscripcion (
+  id_estudiante INT,
+  id_clase INT,
+
+  estado_inscripcion VARCHAR(50),
+  fecha_inscripcion TIMESTAMP,
+
+  PRIMARY KEY (id_estudiante, id_clase),
+  FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id_usuario) ON DELETE CASCADE,
+  FOREIGN KEY (id_clase) REFERENCES Clase(id_clase) ON DELETE CASCADE
 );
 
-CREATE TABLE pago (
+
+CREATE TABLE Pago (
+  id_pago SERIAL PRIMARY KEY,
+  numero_recibo VARCHAR(100) UNIQUE,
+
+  id_clase INTEGER NOT NULL,
+  metodo VARCHAR(50),
+  estado_pago VARCHAR(50),
+  monto NUMERIC(10,2),
+
+  FOREIGN KEY (id_clase) REFERENCES Clase(id_clase) ON DELETE CASCADE
 );
 
-CREATE TABLE reseña (
+
+CREATE TABLE Reseña (
+  id_reseña SERIAL PRIMARY KEY,
+  id_estudiante INTEGER NOT NULL,
+  id_profesor INTEGER NOT NULL,
+
+  puntaje SMALLINT,
+  comentario TEXT,
+  fecha TIMESTAMP DEFAULT now(),
+
+
+  FOREIGN KEY (id_estudiante) REFERENCES Estudiante(id_usuario) ON DELETE CASCADE,
+  FOREIGN KEY (id_profesor) REFERENCES Profesor(id_usuario) ON DELETE CASCADE
 );
+
